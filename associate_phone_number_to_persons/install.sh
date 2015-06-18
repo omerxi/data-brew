e13707d7966e13e091672c5fc021057ed3c22b94
jmvanel <jeanmarc.vanel@gmail.com>
2015-06-18 11:01:21 +0200

    associate_phone_number_to_person (WIP) : replace string <ID> in SPARQL person_to_phone_query.rq
    pave the way for Dockerization in install.sh

---
+++ associate_phone_number_to_persons/install.sh
@@ -0,0 +1,5 @@
+# download & install depdencies : Java 8, nodejs, Apache Jena, Silk
+# Jena cf https://registry.hub.docker.com/u/webratio/ant/dockerfile/
+# cf https://github.com/inutano/bh14-docker-recipes/issues/1
+PATH=~/apps/apache-jena-2.13.0/bin:$PATH
+
--- associate_phone_number_to_persons/run.sh
+++ associate_phone_number_to_persons/run.sh
@@ -1,14 +1,12 @@
-# TODO download & install Apache Jena, Silk
-PATH=~/apps/apache-jena-2.13.0/bin:$PATH
-
 arq --data=../drivers_output.ttl --query idlist.rq --results CSV > idlist.txt
 for ID in `cat idlist.txt`
 do
   echo "For person $ID"
-  # TODO replace string <ID> in person_to_phone_query.rq
+  # replace string <ID> in person_to_phone_query.rq
+  sed -e "1,\$s/<ID>/<${ID}>/" person_to_phone_query.param.rq > person_to_phone_query.rq
   arq --data=../drivers_output.ttl --query person_to_phone_query.rq > /tmp/phone_query.ttl
   turtle --output=JSON-LD /tmp/phone_query.ttl >  /tmp/phone_query.json
   nodejs ../../semantize/fetch/vtc/phones/index.js `cat /tmp/phone_query.json` > /tmp/phone_result.json
-  # TODO launch Silk
+  # TODO turn phone_result.json into JSON-LD ; launch Silk ; exploit Silk result to add phone number to person RDF data.
 done
 
