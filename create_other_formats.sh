014fb8a5c8a0368836a0ef4e2fa74fe2eb35514a
jmvanel <jeanmarc.vanel@gmail.com>
2015-06-25 17:20:55 +0200

    added associate_capacity_to_persons/ ( WIP: slow without SPARQL db)
    added extract_title.rq to clean manager name
    added create_other_formats.sh : creation of JSON-LD, N-Triples, and CSV from Turtle

--- create_other_formats.sh
+++ create_other_formats.sh
@@ -1,5 +1,8 @@
 TTL=associate_phone_number_to_persons/$PERSON_DATA
 
+sort associate_phone_number_to_persons/drivers-phones.nt | uniq > /tmp/drivers-phones.nt
+cp --force /tmp/drivers-phones.nt drivers-phones.nt
+
 arq --data $TTL \
   --data associate_phone_number_to_persons/drivers-phones.nt \
   --query drivers.rq --results CSV > drivers_output.csv
