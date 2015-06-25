504a1ea98a216103d4fe98f76278ea35573d098d
jmvanel <jeanmarc.vanel@gmail.com>
2015-06-25 19:55:23 +0200

    adapt recherche_capacite to SPARQL_remote => recherche_capacite_SPARQL_remote.sh
    Pour info ,   ça va tourner 4 heures pour associer les 4000 personnes morales avec les fichiers capacité par départements  ...     Et pourtant j'utilise la base BlazeGraph ...     Mais j'ai évité de sémantiser les  fichiers capacité , je fait un grep là dedans à chaque VTC .      Bon, mais c'est pas grave , ça tourne :simple_smile:

---
+++ associate_capacity_to_persons/recherche_capacite_SPARQL_remote.sh
@@ -0,0 +1,48 @@
+
+SPARQL_ENDPOINT=http://localhost:9999/bigdata/namespace/omerxi1/sparql
+SPARQL_COMMAND="arq --data=$PERSON_DATA"
+SPARQL_COMMAND="rsparql --service=$SPARQL_ENDPOINT"
+
+HTMLDIR_CAPACITE="$HOME/src/semantize/registre/www2.transports.equipement.gouv.fr/registres/voyageurs"
+
+
+PREFPERSON="oxid"
+echo "@prefix oxi: <http://omerxi.com/ontologies/core.owl.ttl#> ." >> capacite.ttl
+echo "@prefix oxid: <http://omerxi.com/resource/> ." >> capacite.ttl
+
+$SPARQL_COMMAND --query idlist_siren.rq --results CSV > idlist.txt
+echo idlist.txt populated;   wc idlist.txt
+
+for ID in `cat idlist.txt`
+# "EVTC092100010"
+do
+  echo "For person $ID"
+  # remove end of line:
+  ID=${ID:0:`expr length $ID - 1`}
+  if test $ID = "ID" ; then continue ; fi
+  echo "For person <$ID>"
+
+  echo "prefix oxi: <http://omerxi.com/ontologies/core.owl.ttl#> 
+        prefix cob:   <http://cobusiness.fr/ontologies/barter.owl.n3#>
+        prefix oxid: <http://omerxi.com/resource/>
+        prefix foaf:  <http://xmlns.com/foaf/0.1/>
+        CONSTRUCT { ?S ?P ?O . } WHERE { ?S oxi:id \"$ID\" ; ?P ?O . }" > \
+    /tmp/extractbyid.rq
+  $SPARQL_COMMAND --query /tmp/extractbyid.rq --results TURTLE > /tmp/extractbyid.ttl
+  # cat /tmp/extractbyid.ttl
+  SIREN=`grep "cob:siren" /tmp/extractbyid.ttl | sed 's/ *cob:siren *"//'  | sed 's/" *;//'`
+  echo "SIREN <$SIREN>"
+  if test -n "$SIREN" ; then
+    REP=`grep -l "$SIREN" $HTMLDIR_CAPACITE/*htm | sed "s=$HTMLDIR_CAPACITE/==;s=\.htm=="`
+    IDVTC=`grep "oxi:id" /tmp/extractbyid.ttl | sed 's/ *oxi:id *"//'  | sed 's/" *;//'`
+    IDRDF=`grep "oxi:Ride-hailing-driver" /tmp/extractbyid.ttl | sed 's/ *a *oxi:Ride-hailing-driver *;//' `
+# oxid:driver5137  a            oxi:Ride-hailing-driver ;
+    if test -n "$REP" ; then
+      echo "$IDRDF oxi:capacityDepartment \"$REP\" ." >> capacite.ttl
+    fi
+    echo "$IDRDF $IDVTC SIREN $SIREN Départment <$REP>"
+  else
+    echo pas de SIREN
+  fi
+  
+done
